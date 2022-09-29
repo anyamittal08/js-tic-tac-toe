6b4b5914d1332dcd4c843249dc9e136b91592bfe
anyamittal08 <anyamittal08@gmail.com>
2022-09-29 17:28:03 -0400

    fully funcioning unbeatable AI

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const gameInfo = document.querySelector('.game-info');
 
 let gameboard = Array(9).fill(null);
 let nextPlayer = 'X';
+let winningSquares;
 
-let winner = '';
 let end = false;
+
+const scores = {
+    X: 1,
+    O: -1,
+    tie: 0,
+}
 
 const playerMove = (e) => {
     let currentIndex = parseInt(e.target.id)
@@ -16,24 +22,67 @@ const playerMove = (e) => {
     gameboard[currentIndex] = "X";
     e.target.innerText = "X";
     nextPlayer = "O";
-    gameInfo.innerText = checkWinner() ? `${winner} wins!` : checkDraw() ? 'It\'s a draw' : 'Next Player: O';
+    let result = checkWinner();
+    if (result.end) end = true;
+    gameInfo.innerText = result.winner ? `${result.winner} wins!` : result.draw ? 'It\'s a draw' : 'Next Player: O';
     }
     aiMove();
 }
 
 const aiMove = () => {
     if (!end) {
-        let played = false;
-        while (!played) {
-            let randomValidIndex = Math.floor(Math.random() * gameboard.length);
-            if (!gameboard[randomValidIndex]) {
-                gameboard[randomValidIndex] = 'O';
-                document.getElementById(randomValidIndex).innerText = 'O';
-                nextPlayer = 'X';
-                played = true;
-                gameInfo.innerText = checkWinner() ? `${winner} wins!` : checkDraw() ? 'It\'s a draw' : 'Next Player: X'
+        let bestScore = Infinity;
+        let move;
+        for (let i=0; i<9; i++){
+            if (!gameboard[i]) {
+                gameboard[i] = 'O';
+                let score = minimax(gameboard, 0, true);
+                gameboard[i] = null;
+                if (score < bestScore) {
+                    bestScore = score;
+                    move = i;
+                }
             }
-        } 
+        }
+        gameboard[move] = 'O';
+        document.getElementById(move).innerText = 'O';
+        nextPlayer = 'X';
+        let result = checkWinner();
+        if (result.end) end = true;
+        gameInfo.innerText = result.winner ? `${result.winner} wins!` : result.draw ? 'It\'s a draw' : 'Next Player: X';
+    }
+}
+
+const minimax = (board, depth, isMaximizing) => {
+    let result = checkWinner();
+    if (result.winner) {
+        return scores[result.winner];
+    } else if (result.draw) {
+        return scores.tie;
+    }
+
+    if (isMaximizing) {
+        let bestScore = -Infinity;
+        for (let i=0; i<9; i++) {
+            if (gameboard[i] === null) {
+                gameboard[i] = 'X';
+                let score = minimax(board, depth + 1, false);
+                gameboard[i] = null;
+                bestScore = Math.max(score, bestScore);
+            }
+        }
+        return bestScore;
+    } else {
+        let bestScore = Infinity;
+        for (let i=0; i<9; i++) {
+            if (gameboard[i] === null) {
+                gameboard[i] = 'O';
+                let score = minimax(board, depth + 1, true);
+                gameboard[i] = null;
+                bestScore = Math.min(score, bestScore);
+            }
+        }
+        return bestScore;
     }
 }
 
@@ -47,27 +96,30 @@ const checkWinner = () => {
         [2, 5, 8],
         [0, 4, 8],
         [2, 4, 6],
-    ]
+    ];
+
+    let result = {
+        winner: null,
+        draw: false,
+        end: false,
+    };
 
     for (let i = 0; i < winCases.length; i++) {
         let [a, b, c] = winCases[i];
         if (gameboard[a] && gameboard[a] === gameboard[b] && gameboard[a] === gameboard[c]) {
-            winCases[i].forEach((id) => document.getElementById(id).style.color = 'green');
-            winner = gameboard[a];
-            end = true;
-            return true;
+            winningSquares = [a, b, c];
+            result.winner = gameboard[a];
+            result.end = true;
         }
     }
-    return false;
-}
 
-const checkDraw = () => {
-    let isDraw = true
-    gameboard.forEach(square => {
-      if (!square) isDraw = false;
-    })
-    if (isDraw) end = true;
-    return isDraw
+    if (!result.winner) {
+        if (!gameboard.includes(null)) {
+            result.draw = true;
+            result.end = true;
+        }
+    }
+    return result;
 }
 
 const restart = () => {
@@ -92,3 +144,5 @@ squares.forEach((square) => {
 })
 
 document.querySelector('.restart-btn').addEventListener('click', restart);
+
+// tie wins;
